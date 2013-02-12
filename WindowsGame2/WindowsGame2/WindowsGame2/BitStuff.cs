476700c90d531a17ddf9e1ee623ea6a8e0e9bbff
Wenchong Chen <wenchong.chen@outlook.com>
2013-02-12 10:54:02 +0000

    added BitStuff class

---
+++ WindowsGame2/WindowsGame2/WindowsGame2/BitStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame2
+{
+    class BitStuff
+    {
+        private int bits;
+
+        public void SetBit(int bit)
+        {
+
+        }
+
+        public void ClearBit(int bit)
+        {
+
+        }
+
+        public bool CheckBit(int bit)
+        {
+            return false;
+        }
+
+        public void ToString()
+        {
+
+        }
+    }
+}
